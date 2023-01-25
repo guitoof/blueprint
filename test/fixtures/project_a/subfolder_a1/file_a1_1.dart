8aaead22cfb0d4d794ce31dcb23b6ef64f4515b5
Guillaume Diallo-Mulliez <guillaumedm@bam.tech>
2023-01-26 00:08:53 +0100

    :white_check_mark: (fixtures) Fix wrong case import

--- test/fixtures/project_a/subfolder_a1/file_a1_1.dart
+++ test/fixtures/project_a/subfolder_a1/file_a1_1.dart
@@ -1,4 +1,4 @@
 // ignore_for_file: unused_import
 // ignore_for_file: uri_does_not_exist
 
-import 'package:blueprint/subfolder_A2/file_A2_1.dart';
+import 'package:blueprint/subfolder_a2/file_a2_1.dart';
